e91a60e1c8146c24ed462c0ecde73d1a15aaac86
AurelienLeFrancais <eloy.aur@gmail.com>
2023-07-19 11:09:40 -0400

    www

--- Main.py
+++ Main.py
@@ -1,83 +1,148 @@
-#LOGIQUE COLLISION: ASTEROIDE QUI TOUCHE VAISSEAU
 import tkinter as tk
-from ModeleJeu import AireDeJeu, Vaisseau
-from ControleurJeu import Mouvement, Spawns, Shoot, Collision
-from ControlleurMenu import ControleurMenu
 from functools import partial
+from ControleurJeu import Collision, Mouvement, PlayerControl, Shoot, Spawns, ControleurJeu, Verification
+from ControlleurMenu import ControleurMenu, Choix, Enregistrer
+from ModeleJeu import AireDeJeu, Player, Vaiseau
+from ModeleMenu import Niveau
 from threading import Timer
+
 
 if __name__ == "__main__":
 
+    # Affichage du menu et la récuperation des choix
+
+    #Initialisation des objets du jeu
+    jeu = ControleurJeu()
+    enregistrer = Enregistrer()
     menu = ControleurMenu()
+    level = Niveau()
+    choix = Choix()
+    choix.afficherChoixLevel(menu, level, jeu)
+
    
 
     def commencerJeu():
-        couleurTheme = "#41157A"
+        print(menu.commence)
+        if menu.commence:
+            # créer une fenetre tk avec un titre, un background et des dimensions
+            couleurTheme = "#41157A"
             
-        #creation fenetre root
-        root = tk.Tk()
-        root.title("Star Fighter")
-        root.config(background= couleurTheme)
-        root.geometry("510x680")
+            #creation fenetre root
+            root = tk.Tk()
+            root.title("Star Fighter")
+            root.config(background= couleurTheme)
+            root.geometry("510x680")
 
-        # créer un containter et le centrer dans la fenetre tk
-        mainContainer = tk.Frame(root, background= couleurTheme)
-        mainContainer.pack() # pour centrer et donner un padding
+            # créer un containter et le centrer dans la fenetre tk
+            mainContainer = tk.Frame(root, background= couleurTheme)
+            mainContainer.pack() # pour centrer et donner un padding
 
 
-        # créer un titre de jeu et le mettre dans un grid en lui donnant du padding
-        titre = tk.Label(mainContainer, text="StarFighter", fg='#FFFC33', background= couleurTheme)
-        titre.configure(font=("MV Boli", 25, "bold"))
-        titre.grid(column=1, row=0, padx=10, pady=10)
+            # créer un titre de jeu et le mettre dans un grid en lui donnant du padding
+            titre = tk.Label(mainContainer, text="StarFighter", fg='#FFFC33', background= couleurTheme)
+            titre.configure(font=("MV Boli", 25, "bold"))
+            titre.grid(column=1, row=0, padx=10, pady=10)
+            
+            # créer un container pour afficher les statistiques en meme temps du jeu
+            statsContainer = tk.Canvas(mainContainer, height=20, width=450,background= couleurTheme, highlightthickness=0)
+            statsContainer.grid(column=1, row=3, padx=10, pady=5) # pour centrer et donner un padding
 
-        # créer l'aire de jeu et le mettre dans un grid en lui donnant du padding
-        aireDeJeu = AireDeJeu(mainContainer)
-        vaisseau = Vaisseau(aireDeJeu)
-        mvmt = Mouvement()
-        spawns = Spawns()
-        shoot = Shoot()
-        collision = Collision()
+            # créer un container pour afficher le score
+            scoreLabel = tk.Label(statsContainer, text="   Score:  " , fg='#FFFD85',background= couleurTheme)
+            scoreLabel.grid(column=1, row=1, padx=10) # pour centrer et donner un padding
 
-        #----------------------MISE EN PLACE BOUTONS------------------------------------------------------------------
-        # créer un container des buttonset le mettre dans un grid en lui donnant du padding
-        buttonsContainer = tk.Canvas(mainContainer, background= couleurTheme, highlightthickness=0)
-        buttonsContainer.grid(column=1, row=4, padx=10, pady=20) # pour centrer et donner un padding
-        couleurButtons = "#E22866"
-        buttonEnregistrer = tk.Button(buttonsContainer, text="    Enregistrer    ", background= couleurButtons, fg='#FFFED6', font=('arial', 9, 'bold'))#, command=partial(enregistrer.askUsername, player,jeu))
-        buttonEnregistrer.grid(column=1, row=1, padx=15)
+            # créer un container pour afficher la barre de vie
+            hpLabel = tk.Label(statsContainer, text="   Vies:  ", fg='#FFFD85',background= couleurTheme)
+            hpLabel.grid(column=2, row=1, padx=10) # pour centrer et donner un padding
 
-        # créer un button qui affiche le menu score un nouveau jeu et le mettre dans un grid en lui donnant du padding
-        buttonMenuScores = tk.Button(buttonsContainer, text="    Scores    ", background= couleurButtons, fg='#FFFED6', font=('arial', 9, 'bold'))#, command=partial(enregistrer.AfficherScores))
-        buttonMenuScores.grid(column=2, row=1, padx=15)
+            # créer un container des buttonset le mettre dans un grid en lui donnant du padding
+            buttonsContainer = tk.Canvas(mainContainer, background= couleurTheme, highlightthickness=0)
+            buttonsContainer.grid(column=1, row=4, padx=10, pady=20) # pour centrer et donner un padding
+            couleurButtons = "#E22866"
 
-        # créer un button quitte du programme et le mettre dans un grid en lui donnant du padding
-        buttonQuitter = tk.Button(buttonsContainer, text="    Quitter    ", background= couleurButtons, fg='#FFFED6', font=('arial', 9, 'bold'))#, command=root.destroy)
-        buttonQuitter.grid(column=3, row=1, padx=15)
+            # créer un player qui contient les scores et les vies
+            player = Player()
 
-        #------------------------------------------------------------------------------------------------------------
-         # Le vaisseau se deplace en suivant la position de la souris
-        aireDeJeu.canva.bind('<Motion>', partial(mvmt.moveVaisseau, vaisseau, aireDeJeu))
+            #---------------------------------------------------------------------------------------------------------------------------
+            buttonEnregistrer = tk.Button(buttonsContainer, text="    Enregistrer    ", background= couleurButtons, fg='#FFFED6', font=('arial', 9, 'bold'), command=partial(enregistrer.askUsername, player,jeu))
+            buttonEnregistrer.grid(column=1, row=1, padx=15)
 
-         # # Un missile est tiré lorsqu'on fait un click gauche de la souris
-        aireDeJeu.canva.bind('<Button-1>', partial(shoot.shootMissile, aireDeJeu, vaisseau))
+            # créer un button qui affiche le menu score un nouveau jeu et le mettre dans un grid en lui donnant du padding
+            buttonMenuScores = tk.Button(buttonsContainer, text="    Scores    ", background= couleurButtons, fg='#FFFED6', font=('arial', 9, 'bold'), command=partial(enregistrer.AfficherScores))
+            buttonMenuScores.grid(column=2, row=1, padx=15)
 
-         # On bouge le missile vers le haut
-        mvmt.mouvMissiles(aireDeJeu, shoot.listeMissiles, menu.timerMoveMissile)
+            # créer un button quitte du programme et le mettre dans un grid en lui donnant du padding
+            buttonQuitter = tk.Button(buttonsContainer, text="    Quitter    ", background= couleurButtons, fg='#FFFED6', font=('arial', 9, 'bold'), command=root.destroy)
+            buttonQuitter.grid(column=3, row=1, padx=15)
 
-         # creation ovnis
-        spawns.createOvnis(menu.timerCreateOvnis, aireDeJeu)
+            #-------------------------------------------------------------------------------------------------------------------------------
+            
+            # créer l'aire de jeu et le mettre dans un grid en lui donnant du padding
+            aireDeJeu = AireDeJeu(mainContainer)
+            
+            jeu.create_widget(statsContainer) #HUD
+            jeu.startTimer() #Timer, commence en mem
 
-         # creation astroides
-        spawns.createAsteroide(menu.timerCreateAsteroide, aireDeJeu)
 
-         # mouvement des ovnis
-        mvmt.moveOvnis(menu.timerMoveOvnis, menu.vitesseOvniY, menu.vitesseOvniX, spawns.listeOvnis, aireDeJeu)
-         # mouvement des astroides
-        mvmt.moveAsteroide(menu.timerMoveAsteroide, spawns.listAsteroides, aireDeJeu)
+            # controler le player
+            playerControl = PlayerControl(player)
+            verif = Verification()
+            vaisseau = Vaiseau(aireDeJeu)
+            spawns = Spawns()
+            mvmt = Mouvement()
+            shoot = Shoot()
+            collision = Collision()
 
-        collision.verifierCollision(vaisseau, spawns.listeOvnis, shoot.listeMissiles, spawns.listAsteroides)
+            # Le vaisseau se deplace en suivant la position de la souris
+            aireDeJeu.canva.bind('<Motion>', partial(mvmt.moveVaisseau, vaisseau, aireDeJeu, jeu))
 
-        root.mainloop()
+            # # Un missile est tiré lorsqu'on fait un click gauche de la souris
+            aireDeJeu.canva.bind('<Button-1>', partial(shoot.shootMissile, aireDeJeu, vaisseau))
+            # On bouge le missile vers le haut
+            mvmt.mouvMissiles(aireDeJeu, shoot.listeMissiles, menu.timerMoveMissile, jeu)
 
-    
+            # Deux lasers sont tirés lorsqu'on fait un click droit de la souris
+            aireDeJeu.canva.bind('<Button-3>', partial(shoot.shootLaser, aireDeJeu, vaisseau))
+
+            # creation ovnis
+            spawns.createOvnis(menu.timerCreateOvnis, aireDeJeu, jeu)
+
+            # creation astroides
+            spawns.createAsteroide(menu.timerCreateAsteroide, aireDeJeu, jeu)
+
+            # creation powerup
+            spawns.createPU(menu.timerCreatePU, aireDeJeu, jeu)
+
+            # mouvement des ovnis
+            mvmt.moveOvnis(menu.timerMoveOvnis, menu.vitesseOvniY, menu.vitesseOvniX, spawns.listeOvnis, aireDeJeu, jeu)
+
+            # mouvement des astroides
+            mvmt.moveAsteroide(menu.timerMoveAsteroide, spawns.listAsteroides, aireDeJeu, jeu)
+
+            # mouvement des powerup
+            mvmt.movePowerUp(menu.timerMovePU, menu.vitessePU, spawns.listPU, aireDeJeu, jeu)
+
+            # shoot mines des ovnis
+            shoot.shootMine(menu.timerShootMine, spawns.listeOvnis, aireDeJeu, jeu)
+
+            # mouvement de mines
+            mvmt.mouvMines(shoot.listMine, aireDeJeu, jeu)
+                
+            # verifier les collision entre tout les objets du jeu
+            collision.verfierToutesCollisions(vaisseau,spawns.listeOvnis, shoot.listeMissiles ,spawns.listAsteroides, shoot.listMine, shoot.listLaser ,spawns.listPU, level.niveau, player, playerControl, aireDeJeu)    #TODO ajouter collision avec powerup
+
+            # faire raffraichir les stats du player tout a long du jeu
+            playerControl.updatePlayer(scoreLabel, hpLabel)
+
+            # FIN DE PARTIE
+
+            # boocler la fenetre tk
+            verif.verifHP(player, jeu, aireDeJeu)
+            root.mainloop()
+
+            
+        else:
+            commencerJeuTimer = Timer(0.03, commencerJeu)
+            commencerJeuTimer.start()
+
     commencerJeu()
--- ModeleJeu.py
+++ ModeleJeu.py
@@ -1,3 +1,4 @@
+import random
 import tkinter as tk
 
 class AireDeJeu:
@@ -8,27 +9,70 @@ class AireDeJeu:
     def __init__(self, container):
         self.height = 500
         self.width = 450 
-        self.imageBackground = tk.PhotoImage(file='Images/Background.png').subsample(2,2) #Image de fond -> le subsample definit la taille de limage de fond. Attention, plus on laugmente, plus la taille est petite
+        self.imageBackground = tk.PhotoImage(file='Images/Background.png').subsample(2,2) #Image de fond
         
         self.canva = tk.Canvas(container, height=self.height, width=self.width)
         self.canva.create_image(10,10, image=self.imageBackground)
         self.canva.grid(column=1, row=1, padx=20) # pour centrer et donner un padding
         self.canva.config(cursor="none")
+        # self.canva.configure(bg="black")
+        # self.imageBackground = None
+        
 
-class Vaisseau:
+class Player:
+    '''
+    Cette classe initialise un joueur. L'utilisateur commence toujours une partie avec 0 de score et 10 points de vie (HealthPoints)
+    '''
+    def __init__(self):
+        self.score = 0
+        self.hp = 10 #Points de vie
+
+
+class Vaiseau:
     """
     Class Vaiseau permet l'initialisation de l'objet vaisseau, controlable par l'utilisateur
     Parameters: Container, pour pouvoir placer le vaisseau dans l'air de jeu.
     """
-    def __init__(self, container):#dans le container
-        self.imageVaisseau = tk.PhotoImage(file='Images/Vaisseau.png').subsample(6,6) #creer un vaisseau
+    def __init__(self, container):
+        self.imageVaisseau = tk.PhotoImage(file='Images/Vaisseau.png').subsample(6,6)
         self.x = 0
         self.y = 0
+        self.laserCooldown = False
+        self.missileCooldown = False
 
     def setPositions(self,x,y):
         self.x = x
         self.y = y
-        
+
+
+class Missile:
+    """
+    Class Missile permet d'initialiser un objet missile, prend les coordonnes ainsi que le container tkinter comme paramatre.
+    """
+    def __init__(self,container, x, y):
+        self.x = x
+        self.y = y
+        self.imageMissile = tk.PhotoImage(file='Images/missile.png').subsample(3,3)
+        self.instanceMissile = container.canva.create_image(self.x, self.y, image=self.imageMissile)
+
+class Mine:
+    """
+    Class Mines permet d'initialiser un objet mine, prend les coordonnes ainsi que le container tkinter comme paramatre.
+    """
+    def __init__(self,container, x, y):
+        self.x = x
+        self.y = y
+        self.imageMine = tk.PhotoImage(file='Images/mine.png').subsample(7,7)
+        self.instanceMine = container.canva.create_image(self.x, self.y, image=self.imageMine)
+
+class Laser:
+    """
+    Cette classe permet de creer un laser, qui sera tire par l'utilisateur
+    Parameters: Le container tkinter (AirDeJeu) et les coordonnes du laser
+    """
+    def __init__(self,container, xCoinGaucheHaut, yCoinGaucheHaut, xCoinDroitBas, yCoinDroitBas, ):
+        self.rectangleLaser = container.canva.create_rectangle(xCoinGaucheHaut, yCoinGaucheHaut, xCoinDroitBas, yCoinDroitBas, fill="red")
+        self.x = xCoinGaucheHaut + ((xCoinDroitBas - xCoinGaucheHaut)/2) # x c'est la moitie du width du laser
 
 class Ovni:
     '''
@@ -37,10 +81,10 @@ class Ovni:
     def __init__(self,container, x, y):
         self.x = x
         self.y = y
-        self.imageOvni = tk.PhotoImage(file='Images/ovni.png').subsample(4,4) #Creation de l'image Ovni .subsample -> plus cest petit plus cest gros
+        self.imageOvni = tk.PhotoImage(file='Images/ovni.png').subsample(4,4) #Creation de l'image Ovni
         self.instanceOvni = container.canva.create_image(self.x,self.y,anchor=tk.NW,image=self.imageOvni)
         self.direction = "right"
-
+        
 class Asteroide:
     """
     Cette classe permet l'initialisation d'un asteroide dans l'air de jeu,
@@ -57,14 +101,31 @@ class Asteroide:
         self.instanceAsteroide = container.canva.create_image(self.x, self.y, anchor=tk.NW,image=self.imageAsteroide)   #Placer l'image dans le container
         
 
-class Missile:
-    """
-    Class Missile permet d'initialiser un objet missile, prend les coordonnes ainsi que le container tkinter comme paramatre.
-    """
+class PowerUp:
+    '''
+    Cette classe permet d'initialiser les bonus (power up). Elle est utilise dans controleur jeu.
+    '''
     def __init__(self,container, x, y):
         self.x = x
         self.y = y
-        self.imageMissile = tk.PhotoImage(file='Images/missile.png').subsample(3,3)#plus on grossi, plus cest petit
-        self.instanceMissile = container.canva.create_image(self.x, self.y, image=self.imageMissile) #placer limage dans le container      
-
+        # On defini ici, de façon aleatoire (50% de chance), si le bonus est un bonus de vie ou un bonus de score.
+        if random.randint(0,1) == 0:
+            self.type = "Score"
+            self.imagePU = tk.PhotoImage(file='Images/powerUP_Score.png').subsample(7,7) #Creation de l'image Powerup TODO generation aleatoire pour l'image et string? pour differencier quel powerup c'est
+        else:
+            self.type = "Lives"
+            self.imagePU = tk.PhotoImage(file='Images/powerUP_vie.png').subsample(10,10)
+            
+        print(self.type)
+        
+        self.instancePU = container.canva.create_image(self.x,self.y,anchor=tk.NW,image=self.imagePU)
     
+class Explosion:
+    '''
+    Cette classe permet d'initialiser les explosions. Elle est utilise dans controleur jeu.
+    '''
+    def __init__(self,container, x, y):
+        self.x = x
+        self.y = y
+        self.imageExplosion = tk.PhotoImage(file='Images/explosion1.png').subsample(15,15) #Creation de l'image Explosion
+        self.instanceExplosion = container.canva.create_image(self.x,self.y,anchor=tk.NW,image=self.imageExplosion)
